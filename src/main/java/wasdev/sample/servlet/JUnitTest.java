c1db1fbd3b1ca25c73bf5cd3e7ae0a435326f3cb
akollapudi <akollapudi@miraclesoft.com>
2016-11-08 13:04:03 -0500

    Add files via upload

---
+++ src/main/java/wasdev/sample/servlet/JUnitTest.java
@@ -0,0 +1,25 @@
+package wasdev.sample.servlet;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+ 
+ 
+public class JUnitTest {
+	int value;
+ 
+    @Test
+    public boolean testEvenOddNumber(){
+    	boolean result=false;
+        Utility meo = new Utility();
+        value=17;
+        try{
+        assertEquals("10 is a even number", true, meo.isEvenNumber(value));
+       result= true;
+        
+        }catch(Exception e){
+        	System.out.println("Exception-->"+e);
+        }
+        return result;
+    }
+}
