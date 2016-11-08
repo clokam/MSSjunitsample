fecd1bb31049150336041cc888d8437438843611
akollapudi <akollapudi@miraclesoft.com>
2016-11-08 15:45:28 -0500

    Add files via upload

---
+++ src/main/java/wasdev/sample/servlet/Message.java
@@ -0,0 +1,17 @@
+package wasdev.sample.servlet;
+
+public class Message {
+	
+	public void fun1(){
+		System.out.println("Hiiii");
+	}
+public static void main(String[]Args)
+{
+	System.out.println("Hello....");
+	Message m1=new Message();
+	m1.fun1();
+	
+	
+	
+}
+}
