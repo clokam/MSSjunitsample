4ddf23cb83dc6d71cbdc090145bb154b9409b7f2
akollapudi <akollapudi@miraclesoft.com>
2016-11-08 16:35:27 -0500

    Update Message.java

--- src/main/java/wasdev/sample/servlet/Message.java
+++ src/main/java/wasdev/sample/servlet/Message.java
@@ -2,20 +2,23 @@ package wasdev.sample.servlet;
 
 public class Message {
 	
-	public String finResult;
+	public boolean finResult;
 
-
+Message(boolean result){
+	finResult = result;
+}
 	public void fun1(){
 		System.out.println("Hiiii");
 	}
 
-
+	
 public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		boolean finResult=false;
 		JUnitTest jtest= new JUnitTest();
 		finResult=jtest.testEvenOddNumber();
 		System.out.println("Result-->"+finResult);
+		
 		
 
 	}
