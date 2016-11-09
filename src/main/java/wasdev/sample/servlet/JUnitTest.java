d762a41b9b8f8cccf133dea4f52f64039aea9bda
akollapudi <akollapudi@miraclesoft.com>
2016-11-09 04:42:09 -0500

    Update JUnitTest.java

--- src/main/java/wasdev/sample/servlet/JUnitTest.java
+++ src/main/java/wasdev/sample/servlet/JUnitTest.java
@@ -9,13 +9,19 @@ public class JUnitTest {
 	int value;
  
     @Test
-    public boolean testEvenOddNumber(){
+    public boolean testEvenOddNumber() throws Exception{
     	boolean result=false;
         Utility meo = new Utility();
         value=18;
         try{
         assertEquals("10 is a even number", true, meo.isEvenNumber(value));
        result= true;
+		if(result=="true")
+			{
+				System.out.println("Success ");	
+			}else{
+				throw new Exception("fail");
+			}
         
         }catch(Exception e){
         	System.out.println("Exception-->"+e);
